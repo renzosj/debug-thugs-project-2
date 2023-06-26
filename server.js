59859a99b9803ed99eff2ff67fba7cb1ee1803c8
christinahoang32 <christina.hoang@salesforce.com>
2023-06-25 19:31:58 -0700

    adding midddleware

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cookieParser());
+app.use(session({
+  secret: 'your-secret-key', // Replace with your own secret key
+  resave: false,
+  saveUninitialized: true,
+}));
 
-app.
-use(cookieParser());
-
-// app.
-// use(session
-// ({
+// Set up Handlebars as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Start the server
 app.listen(port, () => {
