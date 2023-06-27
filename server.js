91473e00c89c381df10703c29aa77078137f5c4a
christinahoang32 <christina.hoang@salesforce.com>
2023-06-26 22:27:05 -0700

    testing email send feature

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Load environment variables
+require('dotenv').config();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +27,46 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.use('/', require('./controllers/index'));
+app.get('/', (req, res) => {
+  res.render('homepage'); 
+});
+  app.get('/email', (req, res) => {
+    res.render('email');
+  });
+
+
+// Send email route
+app.post('/send-email', (req, res) => {
+  const { email, subject, message } = req.body;
+
+  // Create Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+
+  // Define email options
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: email,
+    subject: subject,
+    text: message
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.redirect('/'); // Redirect to an error page or handle the error accordingly
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.redirect('/'); // Redirect to a success page or handle the success accordingly
+    }
+  });
+});
 
 // Start the server
 app.listen(PORT, () => {
