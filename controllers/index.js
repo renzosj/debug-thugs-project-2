aafc93d4f521dd4278adbc05104e0aec8fd3bc0e
christinahoang32 <christina.hoang@salesforce.com>
2023-07-05 21:33:02 -0700

    SendEmail

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,16 @@
-// Controllers index
+// Christina adding routes and sessions to page
+
 const express = require('express');
 const router = express.Router();
+ 
 
-router.use('/', require ('./homeRoutes/index'));
-router.use('/user', require('./userRoutes/index'));
+// router.use('/user', require('./user'));
+// router.use('/login', require('./login'));
+// router.use('/message', require('./message')); 
+// router.use('/signup', require('./signup'));
+router.use('/user', require('./userRoutes'));
+router.use('/', require ('./homeroutes'));
+
+
 
 module.exports = router;
--- controllers/userRoutes/message.js
+++ controllers/userRoutes/message.js
@@ -11,28 +11,29 @@ router.get('/', withAuth, async (req, res) => {
 
 // Send a Message route (POST)
 router.post('/', withAuth, async (req, res) => {
-
-    const { message_text, chat_id, box_check } = req.body;
-    let delay_send;
-    if (!box_check) {
-        delay_send = false;
-    } else {
-        delay_send = true;
-    }
-    const { userID, username } = req.session;
-    console.log(`\n${userID}: ${username} sends the message: '${message_text}' at chat with ID: ${chat_id} with delay send? ${delay_send}\n`);
     try {
+        const { chat_id, box_check } = req.body;
+        const originalMessageText = req.body.message_text
+        let delay_send;
+        if (!box_check) {
+            delay_send = false;
+        } else {
+            delay_send = true;
+        }
+        const { userID, username } = req.session;
+        console.log(`\n${userID}: ${username} sends the message: '${originalMessageText}' at chat with ID: ${chat_id} with delay send? ${delay_send}\n`);
+
         const newMessageData = await Messages.create({
-            message_text: message_text,
+            message_text: originalMessageText,
             chat_id: chat_id,
             user_id: userID,
             delay_send: delay_send
         })
-        const newMessage = newMessageData.get({ plain: true});
-        //console.log("Your new msg king!: " + newMessage);
-        
+        const newMessage = newMessageData.get({ plain: true });
+        console.log("Your new msg king!: " + newMessage);
+
         // plug in nodemailer 
-        const { email, subject, message_text } = newMessage;
+        const { email, subject, message_text } = req.body;
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -40,7 +41,7 @@ router.post('/', withAuth, async (req, res) => {
                 pass: process.env.GMAIL_PASS
             }
         });
-        
+
         // Define email options
         const mailOptions = {
             from: process.env.GMAIL_USER,
@@ -48,7 +49,7 @@ router.post('/', withAuth, async (req, res) => {
             subject: subject,
             text: message_text
         };
-        
+
         // Send the email
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
@@ -59,12 +60,11 @@ router.post('/', withAuth, async (req, res) => {
                 res.redirect('/'); // Redirect to a success page or handle the success accordingly
             }
         });
-        
-        //res.json(newMessage);
-        res.render('/message');
+
 
         // render sent message page here
     } catch (err) {
+        console.log(err);
         res.json(err);
     }
 });
